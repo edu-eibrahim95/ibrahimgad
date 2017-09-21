d7a7244299f2019cfca8a370ea9263682a94e671
Ibrahim E.Gad <eibrahim95@azhar.edu.eg>
2017-09-22 01:37:55 +0200

    hi

--- app/FacebookUser.php
+++ app/FacebookUser.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class FacebookUser extends Model
 {
-    //
+    protected $attributes = ['access_token' => NULL];
+    protected $fillable = ['facebook_id', 'access_token'];
 }
