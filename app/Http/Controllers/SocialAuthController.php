75f8770b51573b0f24f3a9cf7ad65b66595e6b52
Ibrahim E.Gad <eibrahim95@azhar.edu.eg>
2017-09-21 19:52:11 +0200

    yareet

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -13,9 +13,10 @@ class SocialAuthController extends Controller
 {
     public function redirect()
     {
+    	$accessToken = 'EAAE8eff4LWwBAIibqoZA2tZAbrYSVHNZB2vsrPyZB7A5X0khHtmIhXVDpgBZAAdS6vlI6ZANQRs7lilDayNkhk6KkEjyuxCuMk0wZAfXDMxxHaZAxKetTF52x2JM01Er6b1s0dzbeR27OVrnI3kqWKSpDCBst8i3IVnlpUWQex6E0qg94vZBl8LP2ikt5SnSpIc0ZD';
         return Socialite::driver('facebook')->scopes([
             'email', 'user_managed_groups'
-        ])->redirect();   
+        ])->userFromToken($accessToken);   
     }   
 
     public function callback()
