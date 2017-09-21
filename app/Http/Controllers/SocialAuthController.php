397408ac5827d9bc8cb8263933112e2b8e3a5066
Ibrahim E.Gad <eibrahim95@azhar.edu.eg>
2017-09-22 01:50:21 +0200

    hi

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -19,7 +19,7 @@ class SocialAuthController extends Controller
     public function index(){
         $user = Auth::user();
         $graphNode = Array();
-        if ($user->facebook_id == NULL){
+        if ($user->facebook_id != NULL){
             $fb = new Facebook([
                 'app_id' => config('facebook.config')['app_id'],
                 'app_secret' => config('facebook.config')['app_secret'],
@@ -33,7 +33,7 @@ class SocialAuthController extends Controller
     }
     public function redirect()
     {
-    	$accessToken = 'EAAE8eff4LWwBADTuZCKlVqtAIxRtxFi9QBVDoAK2mlT6uw7XVj6TJ0vOhzbSBvEDojZBKoZCOUTHp9Ogyzgtd1QukeeY6lRB6RmfADCiieVgv33vGVY1NS4vnKnE8lMLZBZA4EBrZCB6p33VJjZCnwtS42uDQseomq1BBc6PWkT6LwcoFSZBTHQpfQn0oqA2vMJ3vHTEF8KdGVRYXOGzHqXCc7LOHAOUTGEZD';
+    	$accessToken = 'EAAE8eff4LWwBAM41R8EzUy9GHRgDoIBaucOBVtOWYIpbUC9xDjjqkNM1qCxQTGbZCHN1AK3fQ7WorZBwPtkphyX3YjD7WaSYK2F2DjOHCSAc0pqyUsF4jsZABYJwkjgZCWPYc3S8IcxTr4eW5GFDSab3y3n6XKgiAAAdfzrfhw0KL5EtNXHh';
         //return Socialite::driver('facebook')->scopes([
           //  'email', 'user_managed_groups'
         //])->userFromToken($accessToken)->getName();
