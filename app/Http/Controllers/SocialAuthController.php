a2f93fddfeacd31ad44107b0ff47f6509ac21b70
Ibrahim E.Gad <eibrahim95@azhar.edu.eg>
2017-09-22 04:10:04 +0200

    hi

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -28,7 +28,6 @@ class SocialAuthController extends Controller
             $access_token = FacebookUser::where('id', $user->facebook_id)->first()['access_token'];
             $start = "17-01-01";
             $prev = $start;
-            return "hi";
             for ($next = date('y-m-d', strtotime($prev. '+1 day')); $next != date('y-m-d');$next=date('y-m-d', strtotime($prev. '+1 day'))){
                 $response = $fb->get('/382982675402366/feed?since='.$prev.'&until='.$next.'&limit=10000', $access_token);
                 $graphEdge = $response->getGraphEdge();
