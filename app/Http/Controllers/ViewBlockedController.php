57301fb766486933de410a3567a00c7486994fd4
Ibrahim E.Gad <eibrahim95@azhar.edu.eg>
2017-09-24 00:31:50 +0200

    hi

--- app/Http/Controllers/ViewBlockedController.php
+++ app/Http/Controllers/ViewBlockedController.php
@@ -8,7 +8,7 @@ class ViewBlockedController extends Controller
 {
     public function medium(){
 		$homepage = file_get_contents('http://www.medium.com/');
-    	return view('welcome', compact('homepage'));
+    	return view('blocked', compact('homepage'));
     }
     public function forUrl($url){
     	$homepage = file_get_contents(str_replace('|', '/', $url));
