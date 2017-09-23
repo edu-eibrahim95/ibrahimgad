6afb1debc9cc51d33618d0adee812539d30a69a0
Ibrahim E.Gad <eibrahim95@azhar.edu.eg>
2017-09-23 23:45:03 +0200

    hi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Ibrahim E.Gad</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
+@extends('layouts.app')
+@section('head')
         <style>
             html, body {
                 background-color: #fff;
@@ -63,11 +52,9 @@
                 margin-bottom: 30px;
             }
         </style>
-    </head>
-    <body>
-
-
-        <div class="flex-center position-ref full-height">
+@endsection
+@section('content')
+    <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -93,7 +80,6 @@
                 </div>
             </div>
         </div>
-    </body>
     <!--<iframe style="width:99%;height:500px;" src="https://medium.com/"><p>Your browser does not support iframes.</p></iframe>-->
     {!! $homepage !!}
-</html>
+@endsection
