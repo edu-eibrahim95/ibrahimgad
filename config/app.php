78c1b70a3fd94472a1c174d528d3313269bdd4c6
Ibrahim E.Gad <eibrahim95@azhar.edu.eg>
2017-12-26 00:51:18 +0200

    crud fix

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
         
+        Backpack\Base\BaseServiceProvider::class,
 
     ],
 
