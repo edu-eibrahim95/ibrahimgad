762180b0eff6596e57ac5c9f64b863e17a052fa0
Ibrahim E.Gad <eibrahim95@azhar.edu.eg>
2017-12-28 23:52:08 +0200

    admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        //if(! file_exists(public_path('uploads'))) {
-            symlink(storage_path('uploads'), public_path('uploads'));
-        //}
     }
 
     /**
